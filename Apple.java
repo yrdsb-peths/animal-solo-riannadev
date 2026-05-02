b450f4c41c4784b721a363d4a69e5ce752b269be
riannadev <riannaknogl@gmail.com>
2026-05-01 22:05:05 -0400

    Update Apple.java

--- Apple.java
+++ Apple.java
@@ -8,10 +8,6 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Apple extends Actor
 {
-    /**
-     * Act - do whatever the Apple wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public void act()
     {
         setLocation(getX(), getY() + 1);
