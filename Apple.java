726e8fb3595b6ab81472a82b5a88ac5cbc17d063
riannadev <riannaknogl@gmail.com>
2026-05-07 10:27:37 -0400

    Update Apple.java

--- Apple.java
+++ Apple.java
@@ -10,6 +10,17 @@ public class Apple extends Actor
 {
     public void act()
     {
-        setLocation(getX(), getY() + 1);
+        // apple falls downwards
+        int x = getX();
+        int y = getY() + 2;
+        setLocation(x, y);
+        
+        //remove apple and draw game over when apple hots bottom
+        MyWorld world = (MyWorld) getWorld();
+        if(getY()<= world.getHeight())
+        {
+            world.gameOver();
+            world.removeObject(this);
+        }
     }
 }
