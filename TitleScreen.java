51ca3e3447bc6c33ed5ac2c2c424fb2c5a1bf7b4
riannadev <riannaknogl@gmail.com>
2026-05-07 22:24:55 -0400

    Create TitleScreen.java

---
+++ TitleScreen.java
@@ -0,0 +1,35 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Title screen
+ * 
+ * @author (Rianna) 
+ * @version (May 2026)
+ */
+public class TitleScreen extends World
+{
+    Label titleLabel = new Label("Hungry Elephant", 60);
+    /**
+     * Constructor for objects of class TitleScreen.
+     * 
+     */
+    public TitleScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        addObject(titleLabel, getWidth()/2, getHeight()/2);
+    }
+    
+    /**
+     * the main world act loop
+     */
+    public void act()
+    {
+        // start the game if the user presses the space bar
+        if(Greenfoot.isKeyDown("space"))
+        {
+            MyWorld gameWorld = new MyWorld();
+            Greenfoot.setWorld(gameWorld);
+        }
+    }
+}
