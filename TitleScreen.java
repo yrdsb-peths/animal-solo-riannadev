d098a35e6eb83c0705469ab3ad606b4855ef2f2b
riannadev <riannaknogl@gmail.com>
2026-05-07 22:29:59 -0400

    Update TitleScreen.java

--- TitleScreen.java
+++ TitleScreen.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class TitleScreen extends World
 {
-    Label titleLabel = new Label("Hungry Elephant", 60);
+    Label titleLabel = new Label("The Elephant", 60);
     /**
      * Constructor for objects of class TitleScreen.
      * 
@@ -17,9 +17,10 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        addObject(titleLabel, getWidth()/2, getHeight()/2);
+        addObject(titleLabel, getWidth()/2, 200);
+        prepare();
     }
-    
+
     /**
      * the main world act loop
      */
@@ -31,5 +32,33 @@ public class TitleScreen extends World
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         }
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Elephant elephant = new Elephant();
+        addObject(elephant,532,94);
+        elephant.setLocation(528,95);
+        elephant.setLocation(525,88);
+        Label label = new Label("Press <space> to Start", 40);
+        addObject(label,364,279);
+        label.setLocation(289,297);
+        elephant.setLocation(84,98);
+        Apple apple = new Apple();
+        addObject(apple,510,69);
+        apple.setLocation(547,181);
+        apple.setLocation(532,108);
+        Label label2 = new Label("Use <-- and --> to Move", 40);
+        addObject(label2,309,263);
+        label2.setLocation(302,244);
+        label.setLocation(319,322);
+        label2.setLocation(297,254);
+        elephant.setLocation(69,69);
+        apple.setLocation(542,74);
+        label.setLocation(292,317);
     }
 }
